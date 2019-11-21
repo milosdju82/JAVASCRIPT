a3771e501acd2c333dcfc4e394076c0bcf8c3825
milosdju82 <djura82@gmail.com>
2019-11-21 22:59:21 +0100

    homework for js uvod, varijabile, operatori

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 
+var peter = 3500;
+var jim = 5000;
+
+console.log(peter > jim); 
+
+
+
+var john = 38;
+var mike = (john += 8) / 2;
+
+console.log("Mike is " + mike + " years old now");
+
+
+
+var FirstFlock = 78 - 12;
+var SecondFlock = 57 + 12 - 4;
+
+console.log(FirstFlock >= SecondFlock);
