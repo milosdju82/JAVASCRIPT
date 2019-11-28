df6a8c0ec9cd5a9703d0a3eb2a4d9b5b4a7aee1c
milosdju82 <djura82@gmail.com>
2019-11-28 17:27:36 +0100

    Data Types & Conditionals Homework

--- main.js
+++ main.js
@@ -1,19 +1,69 @@
+ 
+// TASK 1
 
-var peter = 3500;
-var jim = 5000;
+var a = 15;
+var b = -2;
+var c = 22;
+var d = 0;
+var e = 13;
 
-console.log(peter > jim); 
+if (a > b && a > c && a > d && a > e) {
+    console.log("Larger number is " + a);
+} else if (b > a && b > c && b > d && b > e) {
+    console.log("Larger number is " + b);
+} else if (c > a && c > b && c > d && c > e) {
+    console.log("Larger number is " + c);
+} else if (d > a && d > c && d > b && d > e) {
+    console.log("Larger number is " + d);
+} else {
+    console.log("Larger number is " + e);
+}
 
 
 
-var john = 38;
-var mike = (john += 8) / 2;
+// TASK 2
 
-console.log("Mike is " + mike + " years old now");
+var a = 3;
+var b = -7;
+var c = 2;
+
+if (a * b * c > 0) {
+	console.log("The sign is +");
+} else {
+	console.log("The sign is -");
+}
 
 
 
-var FirstFlock = 78 - 12;
-var SecondFlock = 57 + 12;
+// TASK 3
 
-console.log(FirstFlock >= SecondFlock - 4);
+var statement = "Hello World";
+
+switch (statement) {
+	case "Hello World":
+		console.log("Ciao mondo");
+	case "Hello World":
+		console.log("Hallo Welt");
+	case "Hello World":
+		console.log("Bonjour le monde");
+	case "Hello World":
+		console.log("Hola Mundo");
+	default:
+		console.log("Hello World");
+}
+
+
+
+// TASK 4
+
+var userAge = 30;
+
+var check = userAge >= 28 ? true : false;
+
+if (!check) {
+	console.log("User is less than 28 years old");
+} else if (userAge != 28) {
+	console.log("User is older than 28");
+} else {
+	console.log("User is 28");
+}
