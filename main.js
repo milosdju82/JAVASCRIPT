ea81bc1e987cb668b1fa8fff14dae471afcc7c6f
milosdju82 <djura82@gmail.com>
2019-12-03 19:00:23 +0100

    Array Homework

--- main.js
+++ main.js
@@ -1,69 +1,113 @@
  
 // TASK 1
 
-var a = 15;
-var b = -2;
-var c = 22;
-var d = 0;
-var e = 13;
+var dataOld = [34, true, "Peter", 1992];
+var dataNew = [];
 
-if (a > b && a > c && a > d && a > e) {
-    console.log("Larger number is " + a);
-} else if (b > a && b > c && b > d && b > e) {
-    console.log("Larger number is " + b);
-} else if (c > a && c > b && c > d && c > e) {
-    console.log("Larger number is " + c);
-} else if (d > a && d > c && d > b && d > e) {
-    console.log("Larger number is " + d);
-} else {
-    console.log("Larger number is " + e);
-}
+for (var i = 0; i < dataOld.length; i++) {
+
+	dataNew[i] = dataOld[i];
+
+} console.log(dataNew);
 
 
 
 // TASK 2
 
-var a = 3;
-var b = -7;
-var c = 2;
+var dataOld = [34, true, "Peter", 1992];
+var dataNew = [];
 
-if (a * b * c > 0) {
-	console.log("The sign is +");
-} else {
-	console.log("The sign is -");
-}
+for (var j = dataOld.length - 1; j >= 0; j--) {
+
+	dataNew[dataOld.length - 1 - j] = dataOld[j];
+
+} console.log(dataNew);
 
 
 
 // TASK 3
 
-var statement = "Hello World";
+var dataOld = [34, true, "Peter", 1992];
+var dataNew = [12, "Jack"];
+var i = 0;
 
-switch (statement) {
-	case "Hello World":
-		console.log("Ciao mondo");
-	case "Hello World":
-		console.log("Hallo Welt");
-	case "Hello World":
-		console.log("Bonjour le monde");
-	case "Hello World":
-		console.log("Hola Mundo");
-	default:
-		console.log("Hello World");
-}
+while (i < dataOld.length) {
+
+	dataNew[2 + i] = dataOld[i];
+	i++;
+
+} console.log(dataNew);
 
 
 
 // TASK 4
 
-var userAge = 30;
+var a = [12, 56, 32, 44, 69];
+var b = [88, 7, 13];
+var c = [];
 
-var check = userAge >= 28 ? true : false;
+for (var i = 0; i < a.length; i++) {
 
-if (!check) {
-	console.log("User is less than 28 years old");
-} else if (userAge != 28) {
-	console.log("User is older than 28");
-} else {
-	console.log("User is 28");
+	c[i] = a[i];
+
+} for ( var j = a.length; j < a.length + b.length; j++) {
+
+	c[j] = b[j - a.length];
+
+} console.log(c);
+
+
+
+// TASK 5
+
+var a = [12, 56, 32, 44];
+var b = [88, 7, 13];
+var c = [];
+
+for (var i = 0; i < a.length; i++) {
+
+	c[i * 2] = a[i];
+
+} for (var j = 0; j < b.length; j++) {
+
+	c[j * 2 + 1] = b[j];
+
+} console.log(c);
+
+
+
+// TASK 6
+
+for (var i = 1; i < 7; i++) {
+
+	var stars = "";
+
+	for (var j = 0; j < i; j++) {
+
+		stars += "*";
+
+	} console.log(stars);
 }
+
+
+
+// TASK 7
+
+var stars = "";
+var x = 6;
+var y = 11;
+
+for (var i = 0; i < x; i++) {
+
+	for (var j = 0; j < y; j++) {
+
+		if (j > 0 && j < 10 && i > 0 && i < 5) {
+			stars += " ";
+
+		} else {
+			stars += "*";
+		}
+
+	} stars += "\n";
+
+} console.log(stars);
