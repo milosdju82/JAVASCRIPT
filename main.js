2ffb3824a7ee62db201255447702c958f1e9bd71
milosdju82 <djura82@gmail.com>
2019-12-19 21:14:18 +0100

    Functions Homework

--- main.js
+++ main.js
@@ -1,113 +1,128 @@
- 
+
 // TASK 1
 
-var dataOld = [34, true, "Peter", 1992];
-var dataNew = [];
+function getArray(numbers) {
 
-for (var i = 0; i < dataOld.length; i++) {
+	var reversed = [];
 
-	dataNew[i] = dataOld[i];
+	for (var i = numbers.length - 1; i >= 0; i--) {
+		reversed[numbers.length - 1 - i] = numbers[i];
+	}
 
-} console.log(dataNew);
+	console.log(reversed);
+}
+
+getArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 
 
 // TASK 2
 
-var dataOld = [34, true, "Peter", 1992];
-var dataNew = [];
+function dataType(types) {
 
-for (var j = dataOld.length - 1; j >= 0; j--) {
+	console.log("Type of recived data is " + typeof types)
+}
 
-	dataNew[dataOld.length - 1 - j] = dataOld[j];
-
-} console.log(dataNew);
+dataType("abc");
+dataType(10);
+dataType(false);
 
 
 
 // TASK 3
 
-var dataOld = [34, true, "Peter", 1992];
-var dataNew = [12, "Jack"];
-var i = 0;
+function sixNames(arrNames) {
 
-while (i < dataOld.length) {
+	var longestName = "";
 
-	dataNew[2 + i] = dataOld[i];
-	i++;
+	for (var i = 0; i < arrNames.length; i++) {
+		if (arrNames[i].length > longestName.length) {
+			longestName = arrNames[i];
+		}
+	}
+	console.log("The longest name is " + longestName);
+}
 
-} console.log(dataNew);
+sixNames(["Merkur", "Mars", "Jupiter", "Saturn", "Uran", "Neptun"]);
 
 
 
 // TASK 4
 
-var a = [12, 56, 32, 44, 69];
-var b = [88, 7, 13];
-var c = [];
+function getSecondLowest(list) {
+	var lowest = list[0];
+	var secondLowest = list[1];
 
-for (var i = 0; i < a.length; i++) {
+	if (secondLowest < lowest) {
+		var temp = lowest;
+		lowest = secondLowest;
+		secondLowest = temp;
 
-	c[i] = a[i];
+	} for (var i = 0; i < list.length; i++) {
+		if (list[i] < lowest) {
+			lowest = list[i];
 
-} for ( var j = a.length; j < a.length + b.length; j++) {
-
-	c[j] = b[j - a.length];
-
-} console.log(c);
-
-
-
-// TASK 5
-
-var a = [12, 56, 32, 44];
-var b = [88, 7, 13];
-var c = [];
-
-for (var i = 0; i < a.length; i++) {
-
-	c[i * 2] = a[i];
-
-} for (var j = 0; j < b.length; j++) {
-
-	c[j * 2 + 1] = b[j];
-
-} console.log(c);
-
-
-
-// TASK 6
-
-for (var i = 1; i < 7; i++) {
-
-	var stars = "";
-
-	for (var j = 0; j < i; j++) {
-
-		stars += "*";
-
-	} console.log(stars);
+	} else if (list[i] < secondLowest && list[i] > lowest) {
+		secondLowest = list[i];
+		}
+	}
+	return secondLowest;
 }
 
+function getSecondGreatest(list) {
+	var greatest = list[0];
+	var secondGreatest = list[1];
 
+	if (secondGreatest > greatest) {
+		var temp = greatest;
+		greatest = secondGreatest;
+		secondGreatest = temp;
+	
+	} for (var i = 0; i < list.length; i++) {
+		if (list[i] > greatest) {
+			greatest = list[i];
 
-// TASK 7
-
-var stars = "";
-var x = 6;
-var y = 11;
-
-for (var i = 0; i < x; i++) {
-
-	for (var j = 0; j < y; j++) {
-
-		if (j > 0 && j < 10 && i > 0 && i < 5) {
-			stars += " ";
-
-		} else {
-			stars += "*";
+	} else if (list[i] > secondGreatest && list[i] < greatest) {
+		secondGreatest = list[i];
 		}
+	}
+	return secondGreatest;
+}
 
-	} stars += "\n";
+function getSecondNumbers(list) {
+	var secondLowest = getSecondLowest(list);
+	var secondLargest = getSecondGreatest(list);
 
-} console.log(stars);
+	var exportedValues = [secondLowest, secondLargest];
+
+	console.log(exportedValues);
+
+	return exportedValues;
+}
+
+getSecondNumbers([70, 42, 18, 55, 13, 97, 6, 23]);
+
+
+
+// TASK 5 
+
+function calculate(numsArr, number) {
+
+	var newArr = [];
+
+	for (var i = 0; i < numsArr.length; i++) {
+		if (numsArr[i] > number) {
+			newArr[newArr.length] = numsArr[i];
+		}
+	}
+	return newArr;
+}
+
+function firstFunction(numsArr, number) {
+
+	var biggerNumbers = calculate(numsArr, number);
+
+	console.log(biggerNumbers);
+}
+
+firstFunction([14, 28, 7, 21, 33, 45], 25);
