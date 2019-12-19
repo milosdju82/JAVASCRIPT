fa49c6890472d95e3b2dc698c3ebc96f546d7ca3
milosdju82 <djura82@gmail.com>
2019-12-19 23:00:11 +0100

    corrected task 3

--- main.js
+++ main.js
@@ -13,7 +13,11 @@ console.log("Mike is " + mike + " years old now");
 
 
 
-var FirstFlock = 78 - 12;
-var SecondFlock = 57 + 12;
+var FirstFlock = 78;
+var SecondFlock = 57;
+var MovingBirds = 12;
+
+FirstFlock - MovingBirds;
+SecondFlock + MovingBirds;
 
 console.log(FirstFlock >= SecondFlock - 4);
