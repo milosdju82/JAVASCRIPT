0d157475052400f6bbbd33c883c174bb534ef043
milosdju82 <djura82@gmail.com>
2020-02-22 13:04:34 +0100

    testing homework

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
 // TASK 1
 
-function browserStatus() {
-	if (navigator.onLine) {
-		alert("Browser is online")
-	} else {
-		console.log("Browser is offline")
-	}
-};
+// function browserStatus() {
+// 	if (navigator.onLine) {
+// 		alert("Browser is online")
+// 	} else {
+// 		console.log("Browser is offline")
+// 	}
+// };
 
-browserStatus();
+// browserStatus();
 
 
 
@@ -19,7 +19,7 @@ browserStatus();
 // 	window.onload = function() {
 // 		if (!location.hash) {
 // 			location = location + "#reloaded";
-// 			location.reload(forceGet);
+// 			location.reload();
 // 		} else if (location.hash = "reloaded") {
 // 			alert("Page has been reloaded")
 // 		}
@@ -47,20 +47,93 @@ browserStatus();
 
 // TASK 4
 
-var num = 0;
+// var num = 0;
 
-function incrementNum() {
-	console.log(num);
-	num++;
-	if (num > 15) {
-		clearInterval(int)
-	}
-};
+// function incrementNum() {
+// 	console.log(num);
+// 	num++;
+// 	if (num > 15) {
+// 		clearInterval(int)
+// 	}
+// };
 
-var int = setInterval(incrementNum, 1000);
+// var int = setInterval(incrementNum, 100);
+
+
 
 // TASK 5
 
+// function randomNumber() {
+
+// 	var result = Math.round(Math.random() * 10);
+// 	return result;
+// }
+
+// var call = randomNumber();
+
+
+
 // TASK 6
 
-// TASK 7
+// var users = [
+// {
+// 	name: "Marko",
+// 	age: 26,
+// 	status: "inactive"
+// },
+// {
+// 	name: "Vlada",
+// 	age: 31,
+// 	status: "inactive"
+// },
+// {
+// 	name: "Milos",
+// 	age: 37,
+// 	status: "inactive"
+// },
+// {
+// 	name: "Nikola",
+// 	age: 27,
+// 	status: "inactive"
+// },
+// {
+// 	name: "Ana",
+// 	age: 34,
+// 	status: "inactive"
+// }
+// ];
+
+
+// var userName = prompt("Please enter your name");
+// var loggedInUser;
+// var index;
+
+// function logIn() {
+
+// 	if (users.some(function(user, i) {
+// 		index = i;
+// 		return user.name === userName;
+// 	})) 
+// 	{	
+// 		loggedInUser = users[index];
+// 		loggedInUser.status = "active";
+// 		localStorage.setItem("loggedInUser", loggedInUser.name);
+// 		console.log(loggedInUser.name + " is logged in.");
+
+// 		setTimeout(function() {
+// 			loggedInUser.status = "inactive";
+// 			localStorage.removeItem("loggedInUser");
+// 			console.log(loggedInUser.name + " has logged out.");
+// 		}, 120000);
+// 	} else {
+// 		console.log("User with name " + userName + " does not exist!");
+// 	}
+// };
+
+// logIn();
+
+
+// TASK STRINGS
+
+// var array = ["Peter", "Jo", "Steaven", "Jack", "Jim"];
+
